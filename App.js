3af857235f6787a35c9ee44292cf183e9b6dff10
simpledude1234 <john.jcacob@gmail.com>
2021-10-28 20:08:09 +0100

    Import React Camera Lib
    Library to be used in the project

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { Camera } from 'expo-camera';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
   return (
